a1ec166b3ec6e154667452cab0860c6931a5153c
Luis Gustavo <guleite3@gmail.com>
2022-07-19 15:57:10 -0300

    Security Config

--- backend/src/main/java/com/devsuperior/dsmeta/DsMetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsMetaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsMetaApplication {
+public class DsmetaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsMetaApplication.class, args);
+		SpringApplication.run(DsmetaApplication.class, args);
 	}
 
 }
